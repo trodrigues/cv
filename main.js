5d927b9e8cb64956d4606a28c9321fb33352b03c
Tiago Rodrigues <tiago@top10.com>
2012-11-13 15:29:12 +0000

    setup js section info

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 (function() {
 
 var nav = document.getElementById('nav');
+var mainSections = document.querySelectorAll('#content > section');
 
-function getMainSections() {
-  
+function getInitialSection() {
+  return (window.location.hash.indexOf('#') > -1) ?
+          window.location.hash.replace(/#/, '') :
+          'about';
+}
+
+function setupSections() {
+  var initialSection = getInitialSection(),
+      foundInitial = false;
+
+  for(var i=0, l=mainSections.length; i<l; i++){
+    if(mainSections[i].id === initialSection) {
+      foundInitial = true;
+    } else {
+      mainSections[i].className = 'out-of-view ' + (foundInitial ? 'right' : 'left');
+    }
+  }
 }
 
 function navHandler(ev) {
@@ -12,6 +28,6 @@ function navHandler(ev) {
 }
 
 nav.addEventListener('click', navHandler, false);
-
+setupSections();
 
 }());
