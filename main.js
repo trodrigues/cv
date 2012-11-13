e60b2b2f729c076c24a621ce69a3f35db1b79b72
Tiago Rodrigues <tiago@top10.com>
2012-11-13 15:14:24 +0000

    initial css and js

---
+++ main.js
@@ -0,0 +1,17 @@
+(function() {
+
+var nav = document.getElementById('nav');
+
+function getMainSections() {
+  
+}
+
+function navHandler(ev) {
+  if(ev.target.tagName.toLowerCase() === 'a'){
+  }
+}
+
+nav.addEventListener('click', navHandler, false);
+
+
+}());
